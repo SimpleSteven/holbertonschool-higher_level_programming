251503af5c635b56d56052b26911cd0ad4c7be2e
SimpleSteven <1649@holbertonschool.com>
2021-01-05 00:16:20 -0500

    Print the error in standard error.

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
+import sys
+
 
 def safe_function(fct, *args):
     try:
         a = fct(*args)
     except (ZeroDivisionError, IndexError) as error:
-        print("Exception: " + str(error))
+        sys.stderr.write("Exception: " + str(error))
         return None
     return a
