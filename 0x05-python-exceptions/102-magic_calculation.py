271d2843b14d8a8577c420b7af6f585d66410565
SimpleSteven <1649@holbertonschool.com>
2021-01-05 17:13:31 -0500

    Function in progress.

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-
-
+import dis
 def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if a > i:
+                raise Exception("Too far")
+            else:
+                result += a ** b / i
+        except ZeroDivisionError:
+            result = a + b
+
+
+dis.dis(magic_calculation)
