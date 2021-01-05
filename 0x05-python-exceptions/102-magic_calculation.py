4c30db26e0f81722f245d6432b9357dad49afe7e
SimpleSteven <1649@holbertonschool.com>
2021-01-05 17:20:37 -0500

    Do some strange calculations.

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,8 +8,9 @@ def magic_calculation(a, b):
                 raise Exception("Too far")
             else:
                 result += a ** b / i
-        except ZeroDivisionError:
+        except:
             result = a + b
+    return result
 
 
 dis.dis(magic_calculation)
