ea0861a4055dadc794bff84ffb6b3bce156693ed
SimpleSteven <1649@holbertonschool.com>
2021-01-05 20:02:01 -0500

    Edge case hold it. Don't delete nothing, if the key isn't finded in the dictionary. Just return the dictionary unchanged.

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python3d
 
 
 def simple_delete(a_dictionary, key=""):
-    a_dictionary.pop(key)
+    if key in a_dictionary:
+        a_dictionary.pop(key)
     return a_dictionary
