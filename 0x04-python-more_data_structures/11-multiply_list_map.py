5902bd3d79aa65c1936e116e305e93dba74d6935
SimpleSteven <1649@holbertonschool.com>
2021-01-06 11:12:56 -0500

    In progress...

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    def multiply_by_2(num1):
-        return num1 * number
-    return list(map(multiply_by_2, my_list))
+    return list(map()
