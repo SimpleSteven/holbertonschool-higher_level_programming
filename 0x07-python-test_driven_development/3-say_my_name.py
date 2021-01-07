b6b48a9b58455441351d49044ceda859ffd7540c
SimpleSteven <1649@holbertonschool.com>
2021-01-07 11:03:20 -0500

    A function that print the first and last name passed as string parameters

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/bin/usr/python3
+'''
+    A function that print the first
+    and last name passed as string parameters
+    Functions:
+        matrix_divided()
+'''
+
+
+def say_my_name(first_name, last_name=""):
+    '''
+    Args:
+        first_name: a string name
+        last_name: a string last name
+    Raises:
+        TypeError:
+        if first_name or last_name aren't strings
+    '''
+
+    if type(first_name) not in [str]:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) not in [str]:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
