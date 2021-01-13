b8c2570a00d18388ccace3081a78782ab989e2ed
SimpleSteven <1649@holbertonschool.com>
2021-01-13 18:19:54 -0500

    Just simple coding answers.

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module to find the max integer in a list
+"""
+import random
+
+# def matrix_mul(m_a, m_b):
+
+
+print(random.randint(1, 10))
---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string():
+    current = 0
+    return f"{'Holberton, ' * (i - 1)}Holberton"
