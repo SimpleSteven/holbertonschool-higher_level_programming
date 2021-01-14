6dae9e3072dc4e46eee947b3bdf51512361476a7
SimpleSteven <1649@holbertonschool.com>
2021-01-14 13:49:51 -0500

    Change int_key from int, to unsigned int.

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,7 +10,7 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	int int_key = hash_djb2(key);
+	unsigned int int_key = hash_djb2(key);
 	return (int_key % size);
 }
 
