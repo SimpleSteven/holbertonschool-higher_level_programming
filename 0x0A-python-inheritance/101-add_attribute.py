60aac8de8790483b216e76c463fe53cbfb14f57c
SimpleSteven <1649@holbertonschool.com>
2021-01-19 16:25:24 -0500

    Goal clear.

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,5 +6,7 @@ def add_attribute(a_class, name, value):
     '''Create a function that add a new attribute to a class. If fail,
     raise an error with a customized message.'''
 
-    if not (setattr(a_class, name, value)):
+    try:
+        setattr(a_class, name, value)
+    except AttributeError:
         raise TypeError("can't add new attribute")
