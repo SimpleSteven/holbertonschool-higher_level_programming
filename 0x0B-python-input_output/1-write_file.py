27250e983d1786d5ec6d2fa9663cebd66efc8bc8
SimpleSteven <1649@holbertonschool.com>
2021-01-20 17:43:20 -0500

    Pepe8 errors fixed.

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''Write a function that writes a string to a text file (UTF8) and returns the number of characters written'''
+'''Write a function that writes a string to a text
+file (UTF8) and returns the number of characters written'''
+
 
 def write_file(filename="", text=""):
     '''With open, in mode "w" and with write, add a text to a file'''
