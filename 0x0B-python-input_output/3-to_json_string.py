18e87ac4eb1797385fad645f4d94bddadbc83815
SimpleSteven <1649@holbertonschool.com>
2021-01-20 21:20:26 -0500

    First convert the object to json, then return it.

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,4 +7,5 @@ representation of an object (string)'''
 def to_json_string(my_obj):
     ''' Convert the object passed, to a json
     representation and return it'''
-    return json.dumps(my_obj)
+    new_json = json.dumps(my_obj)
+    return new_json
