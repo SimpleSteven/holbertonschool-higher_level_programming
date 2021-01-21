584fea9e43ead3d158f233db308fcab6413141a0
SimpleSteven <1649@holbertonschool.com>
2021-01-20 23:01:47 -0500

    Now it works even if the json file is empty.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,6 +3,10 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args_to_json = list(load_from_json_file('add_item.json'))
-args_to_json.append([arg for arg in sys.argv[1:]])
-save_to_json_file(args_to_json, 'add_item.json')
+args_from_json = []
+with open('add_item.json') as file:
+    if not file.read() == '':
+        args_from_json = list(load_from_json_file('add_item.json'))
+args_to_json = [arg for arg in sys.argv[1:]]
+args_from_json.extend(args_to_json)
+save_to_json_file(args_from_json, 'add_item.json')
