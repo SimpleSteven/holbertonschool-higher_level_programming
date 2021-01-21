34cd92cece2e79a5b972d6e8b583935f9a81f9d3
SimpleSteven <1649@holbertonschool.com>
2021-01-20 22:14:51 -0500

    Undo the previous change. Now is, write mode instead of append.

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 def save_to_json_file(my_obj, filename):
     ''' Run open with write, and write into the file,
     a json serialization of an object'''
-    with open(filename, mode='a') as file:
+    with open(filename, mode='w') as file:
         json.dump(my_obj, file)
