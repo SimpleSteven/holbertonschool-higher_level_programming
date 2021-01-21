9d3c9597d3abbb71a3935384cec65464abe592ca
SimpleSteven <1649@holbertonschool.com>
2021-01-20 21:36:17 -0500

    Pep8 errors fixed.

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -3,6 +3,7 @@
 (Python data structure) represented by a JSON string'''
 import json
 
+
 def from_json_string(my_str):
     ''' With json.loads, return a json string
     in python data structure'''
