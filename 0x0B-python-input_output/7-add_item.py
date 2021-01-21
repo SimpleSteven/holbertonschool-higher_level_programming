eb87d498d117c7f866f031ff25dcb46f9931ac43
SimpleSteven <1649@holbertonschool.com>
2021-01-20 23:22:19 -0500

    Now it works even if the json file doesn't exist but without use the open modes.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import sys
+import os.path
+from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args_from_json = []
-with open('add_item.json', mode='a') as file:
-    file.write('')
-with open('add_item.json') as file:
-    if not file.read() == '':
-        args_from_json = list(load_from_json_file('add_item.json'))
+if path.exists('add_item.json'):
+    with open('add_item.json') as file:
+        if not file.read() == '':
+            args_from_json = list(load_from_json_file('add_item.json'))
 args_to_json = [arg for arg in sys.argv[1:]]
 args_from_json.extend(args_to_json)
 save_to_json_file(args_from_json, 'add_item.json')
