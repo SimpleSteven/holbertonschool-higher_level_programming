a5a075b2b4f31dd0826c011dc328222e676e2e46
SimpleSteven <1649@holbertonschool.com>
2021-02-01 14:11:45 -0500

    All methods documented.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,33 +18,49 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        ''' **** The getter of the private ****
+            **** instance attribute width **** '''
         return self.__width
 
     @width.setter
     def width(self, width):
+        ''' **** The setter of the private ****
+            **** instance attribute width **** '''
         self.__width = width
 
     @property
     def height(self):
+        ''' **** The getter of the private ****
+            **** instance attribute height **** '''
         return self.__height
 
     @height.setter
     def height(self, height):
+        ''' **** The setter of the private ****
+            **** instance attribute height **** '''
         self.__height = height
 
     @property
     def x(self):
+        ''' **** The getter of the private ****
+            **** instance attribute x **** '''
         return self.__x
 
     @x.setter
     def x(self, x):
+        ''' **** The setter of the private ****
+            **** instance attribute x **** '''
         self.__x = x
 
     @property
     def y(self):
+        ''' **** The getter of the private ****
+            **** instance attribute y **** '''
         return self.__y
 
     @y.setter
     def y(self, y):
+        ''' **** The setter of the private ****
+            **** instance attribute y **** '''
         self.__y = y
 0
