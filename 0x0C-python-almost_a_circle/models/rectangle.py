f790db81f8e1ece25cfbc43f7e38363b31a959bc
SimpleSteven <1649@holbertonschool.com>
2021-02-01 14:17:45 -0500

    Now the constructor is documented too.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,6 +10,7 @@ class Rectangle(Base):
         **** width, height, y, x and id **** '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        ''' **** Constructor of the Rectangle ****'''
         super().__init__(id)
         self.width = width
         self.height = height
@@ -26,6 +27,8 @@ class Rectangle(Base):
     def width(self, width):
         ''' **** The setter of the private ****
             **** instance attribute width **** '''
+        if type(width) != int:
+            raise TypeError("width must be an integer")
         self.__width = width
 
     @property
@@ -38,6 +41,8 @@ class Rectangle(Base):
     def height(self, height):
         ''' **** The setter of the private ****
             **** instance attribute height **** '''
+        if type(height) != int:
+            raise TypeError("height must be an integer")
         self.__height = height
 
     @property
@@ -50,6 +55,8 @@ class Rectangle(Base):
     def x(self, x):
         ''' **** The setter of the private ****
             **** instance attribute x **** '''
+        if type(x) != int:
+            raise TypeError("x must be an integer")
         self.__x = x
 
     @property
@@ -62,5 +69,6 @@ class Rectangle(Base):
     def y(self, y):
         ''' **** The setter of the private ****
             **** instance attribute y **** '''
+        if type(y) != int:
+            raise TypeError("y must be an integer")
         self.__y = y
-0
