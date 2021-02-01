3f7921cce809a61bd416cb43f4ac1e9f3019dd0c
SimpleSteven <1649@holbertonschool.com>
2021-02-01 17:48:15 -0500

    A new Square class that inherits from Rectangle and slightly change the __str__ dunder funtion.

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' A new class Square, that
+    inherits from Rectangle '''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    ''' The Square Class
+        Args:
+            size: the width and height of the Square
+            y: the y variable
+            x: the x variable
+            id: the id of the instance
+    '''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                 self.y, self.height)
