73875a7cf5c78583edebd05cc4147ad863ed90ee
SimpleSteven <1649@holbertonschool.com>
2021-02-01 15:28:16 -0500

    Update the display method, so now it's taking care of x and y.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,7 +24,7 @@ class Rectangle(Base):
     def display(self):
         ''' Print in stdout the rectangle
             with the "#" character '''
-        print(("#" * self.width + '\n') * self.height, end='')
+        print('\n' * self.y + (' ' * self.x + "#" * self.width + '\n') * self.height, end='')
 
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
