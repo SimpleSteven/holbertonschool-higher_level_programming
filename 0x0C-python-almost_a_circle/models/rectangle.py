fa48404e6eb98cface17130388cae06e7bd4fb34
SimpleSteven <1649@holbertonschool.com>
2021-02-01 14:40:37 -0500

    All documention deleted but class's and module's.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,17 +5,16 @@ from base import Base
 
 
 class Rectangle(Base):
-    ''' The Rectangle Class '''
+    ''' The Rectangle Class
+        Args:
+            width: the width of the rectangle
+            height: the height of the rectangle
+            y: the y variable
+            x: the x variable
+            id: the id of the instance
+    '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        ''' Constructor of the Rectangle class
-            Args:
-                width: the width of the rectangle
-                height: the height of the rectangle
-                y: the y variable
-                x: the x variable
-                id: the id of the instance
-        '''
         self.width = width
         self.height = height
         self.x = x
@@ -24,14 +23,10 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        ''' **** The getter of the private ****
-            **** instance attribute width **** '''
         return self.__width
 
     @width.setter
     def width(self, width):
-        ''' **** The setter of the private ****
-            **** instance attribute width **** '''
         if type(width) != int:
             raise TypeError("width must be an integer")
         if width < 1:
@@ -40,14 +35,10 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        ''' **** The getter of the private ****
-            **** instance attribute height **** '''
         return self.__height
 
     @height.setter
     def height(self, height):
-        ''' **** The setter of the private ****
-            **** instance attribute height **** '''
         if type(height) != int:
             raise TypeError("height must be an integer")
         if height < 1:
@@ -56,14 +47,10 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        ''' **** The getter of the private ****
-            **** instance attribute x **** '''
         return self.__x
 
     @x.setter
     def x(self, x):
-        ''' **** The setter of the private ****
-            **** instance attribute x **** '''
         if type(x) != int:
             raise TypeError("x must be an integer")
         if x < 0:
@@ -72,14 +59,10 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        ''' **** The getter of the private ****
-            **** instance attribute y **** '''
         return self.__y
 
     @y.setter
     def y(self, y):
-        ''' **** The setter of the private ****
-            **** instance attribute y **** '''
         if type(y) != int:
             raise TypeError("y must be an integer")
         if y < 0:
