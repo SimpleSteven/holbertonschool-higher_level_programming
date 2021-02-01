57a92e01818dce7a2cbcba8f273438bc16b04dab
SimpleSteven <1649@holbertonschool.com>
2021-02-01 18:06:39 -0500

    Implement the setter and getter for size, handling width and height that must be both, equal to size.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,7 +15,18 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(width=size, height=size, x=x, y=y, id=id)
+        self.size = size
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
                                                  self.y, self.height)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
+        self.__size = size
